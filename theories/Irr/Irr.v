486b58406d772358a8164a06eb0526c26b824a0a
Pierre <pierre.boutillier@ens-lyon.org>
2013-03-28 17:17:17 +0100

    Here is K

--- theories/Irr/Irr.v
+++ theories/Irr/Irr.v
@@ -10,45 +10,20 @@ Definition ireflTY A (a : A) : Irr := iJMeq a a.
 
 Axiom irefl : forall {A} a, iPrf (ireflTY A a).
 Axiom iAll : forall (S : Type) (P : S -> Irr), Irr.
-Definition casK A a (p : iPrf (ireflTY A a)): p = irefl a :=
-  eq_refl.
+Definition iK A a (p : iPrf (ireflTY A a)): iPrf (iJMeq p (irefl a)) := irefl _.
 
-Fixpoint iplus_n_Sm (m n : nat) : iPrf (iJMeq (m + S n) (S m + n)) :=
-match m with
-  | 0 => irefl (S n)
-  | S m' => isubst _ _ (iplus_n_Sm m' n) (fun x : nat => iPrf (iJMeq (S m' + S n) (S x)))
-    (irefl (S m' + S n))
-end.
-
-Require Plus.
-
-Lemma iplus_tail_plus m : forall n, iPrf (iJMeq (Plus.tail_plus m n) (m + n)).
+Definition compK {X} (x: X) (q: x = x): q = eq_refl.
 Proof.
-induction m.
-exact irefl.
-intro n.
-refine (isubst _ _ (iplus_n_Sm m n)
-  (fun x : nat => iPrf (iJMeq (Plus.tail_plus (S m) n) x)) _).
-exact (IHm (S n)).
-Qed.
+assert (forall (x': X) (q': x = x'), iPrf (iJMeq x x') -> iPrf (iJMeq q q') -> q = eq_refl).
+{ destruct q'. intros ieq_x ieq_q. exact (isubst _ _ ieq_q _ eq_refl). }
+apply (H x q); exact (irefl _).
+Defined.
 
-Require Vector.
-Import Vector.VectorNotations.
-Print Vector.rev_append.
-
-Definition irev_append {A : Type} {n p : nat} (v : Vector.t A n) (w : Vector.t A p) :=
-isubst  (Plus.tail_plus n p) (n + p) (iplus_tail_plus n p) (Vector.t A) (Vector.rev_append_tail v w).
+Eval lazy in (fun X (x: X) => compK x eq_refl).
 
 Section test_suite.
 Fail Definition silly := iPrf (True).
 Fail Check (eq_refl:1 = 2).
-
-Variable xx : nat.
-Variable v1 v2 : Vector.t nat xx.
-Variable v'1 v'2 : Vector.t nat 2.
-Eval lazy in (Vector.rev_append (0 :: v'1) v'2).
-Eval lazy in (irev_append (0 :: v1) v2).
-Eval lazy in (irev_append (0 :: v'1) v'2).
 
 Definition useless m n (f : Fin.t (S (n + m))) := isubst _ _ (irefl (S (n + m))) Fin.t f.
 
